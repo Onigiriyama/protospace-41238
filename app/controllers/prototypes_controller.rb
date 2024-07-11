4ac53bba77529ed56a9cd437ea9e553a8e9ea41f
Onigiriyama <curfews.hinge.00@icloud.com>
2024-07-11 16:20:50 +0900

    詳細ページ修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,6 +24,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @prototype_user = @prototype.user
   end
 
   def edit
